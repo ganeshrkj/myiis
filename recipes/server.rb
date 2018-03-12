7cd4e0a924aeecbeb80bb524fb86c05d3948cefb
ganeshrkj <ganeshrkj@gmail.com>
2018-03-12 20:37:42 +0000

    Version 0.2.0, display system info within webpage content

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,12 +1,7 @@
-powershell_script 'Install IIS' do
-  code 'Add-WindowsFeature Web-Server'
-  action :run
-end
-
 file 'c:\inetpub\wwwroot\Default.htm' do
-  content '<h1>Hello, world!</h1>'
-end
-
-service 'w3svc' do
-  action [:enable, :start]
+  content "<h1>Hello, world!</h1>
+  <h2>PLATFORM: #{node['platform']}</h2>
+  <h2>HOSTNAME: #{node['hostname']}</h2>
+  <h2>MEMORY:   #{node['memory']['total']}</h2>
+  <h2>CPU Mhz:  #{node['cpu']['0']['mhz']}</h2>"
 end
